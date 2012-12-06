8a8a53adbcf3bdc49573d84b31abe03018a08266
Alexander Schlueter <arleckshunt@googlemail.com>
2012-12-06 15:37:04 +0100

    basic layout of MainWindow, load symbol images in constructor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,52 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow() : dis(0, yFlaps - 1), scene(QApplication::desktop()->screenGeometry()), view(&scene),
+    background(":/images/blue_sky.jpg")
 {
-    ui->setupUi(this);
+    QPixmap allSymbols(":/images/flipboardletters.svg");
+    std::array<QPixmap, 65> singleSymbols;
+
+    auto i = 0;
+    for (auto y = 0; y < 7; y++) {
+        int xMax;
+        if (y == 2) xMax = 9;
+        else  if (y == 6) xMax = 6;
+        else xMax = 10;
+        for (auto x = 0; x < xMax; x++)
+            singleSymbols[i++] = allSymbols.copy(x * 135, y * 182, 95, 150).scaled((scene.width() - 2 * xFlapBorder) / xFlaps,
+                                                                                   scene.height() * 3 / (5 * yFlaps));
+    }
+
+    for (auto y = 0; y < yFlaps; y++) {
+        for (auto x = 0; x < xFlaps; x++) {
+            for (auto s = 0; s < symbolCount; s++) {
+                auto item = scene.addPixmap(singleSymbols[s]);
+                item->setPos(xFlapBorder + x * singleSymbols[s].width(), scene.height() / 5 + y * singleSymbols[s].height());
+                flapStacks[y][x][s] = item;
+            }
+        }
+    }
+
+    scene.setBackgroundBrush(background);
+
+    connect(&screenSwitchTimer, SIGNAL(timeout()), SLOT(switchScreen()));
+    screenSwitchTimer.start(screenSwitchInterval);
+
+    view.setWindowTitle(QT_TRANSLATE_NOOP(QGraphicsView, "AbiCountdown 2013"));
+    view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+    view.setCacheMode(QGraphicsView::CacheBackground);
+    view.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    view.showFullScreen();
 }
 
-MainWindow::~MainWindow()
+void MainWindow::switchScreen()
 {
-    delete ui;
+
+    for (auto y = 0; y < yFlaps; y++) {
+
+        for (auto x = 0; x < xFlaps; x++) {
+
+        }
+    }
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,22 +1,41 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <array>
+#include <random>
+#include <chrono>
+#include <QtGui>
+#include <QDate>
 
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow
+class MainWindow : public QObject
 {
     Q_OBJECT
     
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    MainWindow();
     
 private:
-    Ui::MainWindow *ui;
+    static constexpr auto ourFlight = "ABI13", ourGate = "G9";
+    static constexpr auto screenSwitchInterval = 30, animationTime = 1, xFlaps = 20, yFlaps = 5, symbolCount = 65;
+    static constexpr auto xFlapBorder = 40;
+
+    std::array<char, symbolCount> symbols;
+    std::mt19937 gen;
+    std::uniform_int_distribution <> dis;
+
+
+    QTimer screenSwitchTimer;
+
+    typedef std::array<std::array<std::array<QGraphicsPixmapItem*, symbolCount>, xFlaps>, yFlaps> flapStacksT;
+
+    flapStacksT flapStacks;
+
+    QGraphicsScene scene;
+    QGraphicsView view;
+    QPixmap background;
+
+private slots:
+    void switchScreen();
 };
 
 #endif // MAINWINDOW_H
